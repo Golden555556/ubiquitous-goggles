6cbb7680e98ba7ce90baf4f916172cbd8dcdef9b
Golden555556 <krystian5.luczak@gmail.com>
2025-01-29 20:25:06 +0100

    maciek jest super

---
+++ Klasa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kryst on 29.01.2025.
+//
+
+#include "Klasa.h"
---
+++ Klasa.h
@@ -0,0 +1,16 @@
+//
+// Created by kryst on 29.01.2025.
+//
+
+#ifndef KLASA_H
+#define KLASA_H
+
+
+
+class Klasa {
+
+};
+
+
+
+#endif //KLASA_H
